0b7f3a41648d3ad90f939cad3801cbe46f46d841
sid <sidengineer.014@gmail.com>
2025-01-14 23:29:07 +0530

    final project 2

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,10 +1,7 @@
 
 import { NextResponse } from "next/server";
 
-export async function GET(
-    request: Request,
-    { params }: { params: { id: string } }
-) {
+export async function GET({ params }: { params: { id: string } }) {
 
     if (!params.id || isNaN(Number(params.id))) {
         return NextResponse.json({ error: "Invalid product ID" }, { status: 400 });
