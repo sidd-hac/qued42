d5dbb3d1515e7ac7df5a308cc8af7c0b40327db8
sid <sidengineer.014@gmail.com>
2025-01-14 23:48:41 +0530

    final project 5

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -3,15 +3,16 @@ import { NextResponse } from "next/server";
 
 export async function GET(
     request: Request,
-    { params }: { params: { id: string } }
+    {params}: {params: Promise<{ id: number }>}
 ) {
+    const { id } = await params;
 
-    if (!params.id || isNaN(Number(params.id))) {
+    if (!id || isNaN(Number(id))) {
         return NextResponse.json({ error: "Invalid product ID" }, { status: 400 });
     }
 
     try {
-        const response = await fetch(`https://fakestoreapi.com/products/${params.id}`);
+        const response = await fetch(`https://fakestoreapi.com/products/${id}`);
         const data = await response.json();
         return NextResponse.json(data);
     } catch {
